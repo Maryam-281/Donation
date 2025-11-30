072a0dcdc8b15a717c09b7ba609d4d622f6949fb
Maryam-281 <202300485@student.polytechnic.bh>
2025-11-30 11:14:34 +0300

    Initial Commit

---
+++ Donation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Donation
+//
+//  Created by BP-36-201-09 on 30/11/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
