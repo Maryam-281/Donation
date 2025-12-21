1718d20819f14c22098fe589cc6d9ef3cbeb08ee
BP-36-201-07 <bp-36-201-07@BP-36-201-07s-iMac.local>
2025-12-21 12:10:51 +0300

    Created MessagesListView and MessagesRowView, requires data of accounts to confirm working.

---
+++ Donation/Messages/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Donation
+//
+//  Created by BP-36-201-07 on 21/12/2025.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
