74d8a65476cbc222b3fd66fe22ee5af28c6e0fc8
BP-36-201-17 <bp-36-201-17@BP-36-201-17s-iMac.local>
2025-12-23 13:08:06 +0300

    Added Login and Register pages to ensure messaging system works.

---
+++ Donation/Messages/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,49 @@
+//
+//  LoginViewController.swift
+//  Donation
+//
+//  Created by BP-36-201-17 on 23/12/2025.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Log in"
+        view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register",
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(didTapRegister))
+        
+        //Adding Subviews
+        view.addSubview(imageView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let size = view.width/3
+        imageView.frame = CGRect(x: (view.width-size)/2,
+                                 y:20,
+                                 width:size,
+                                 height:size)
+    }
+
+    @objc private func didTapRegister() {
+        let vc = RegisterViewController()
+        vc.title = "Create Account"
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+}
---
+++ Donation/Messages/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterViewController.swift
+//  Donation
+//
+//  Created by BP-36-201-17 on 23/12/2025.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+ 
+}
