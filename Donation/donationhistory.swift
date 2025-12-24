797fcbec5bcf3a1fe1a846eec582c9223a36e350
H4mood99 <202204564@student.polytechnic.bh>
2025-12-24 16:59:18 +0300

    admin dashboard

---
+++ Donation/donationhistory.swift
@@ -0,0 +1,23 @@
+//
+//  donationhistory.swift
+//  Donation
+//
+//  Created by BP-36-201-18 on 24/12/2025.
+//
+
+import UIKit
+
+class donationhistory: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
