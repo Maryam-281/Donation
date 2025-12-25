8c068c529d3b2842f6e4a772dc951b0f01173925
H4mood99 <202204564@student.polytechnic.bh>
2025-12-25 15:56:25 +0300

    supabase

--- Donation/donation.swift
+++ Donation/donation.swift
@@ -1,13 +1,13 @@
 //
-//  donationhistory.swift
+//  donation.swift
 //  Donation
 //
-//  Created by BP-36-201-18 on 24/12/2025.
+//  Created by BP-36-201-18 on 25/12/2025.
 //
 
 import UIKit
 
-class donationhistory: UITableViewCell {
+class donation: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
