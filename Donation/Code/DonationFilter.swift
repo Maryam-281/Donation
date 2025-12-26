ad291f9df5d5c444efb65a4cc615dfa1b0b9834f
maryam-281 <202300485@student.polytechnic.bh>
2025-12-26 17:00:09 +0300

    hihhihiihhi

--- Donation/Code/DonationFilter.swift
+++ Donation/Code/DonationFilter.swift
@@ -5,32 +5,3 @@
 //  Created by BP-36-201-09 on 24/12/2025.
 //
 
-import Foundation
-
-enum Category: String, CaseIterable {
-    case preparedMeals = "Prepared Meals"
-    case fruitsVegetables = "Fruits & Vegetables"
-    case bakedGoods = "Baked Goods"
-    case drinks = "Drinks"
-    case dairyProducts = "Dairy Products"
-}
-
-enum Location: String, CaseIterable {
-    case muharraq = "Muharraq"
-    case northern = "Northern Governate"
-    case southern = "Southern Governate"
-    case manama = "Manama"
-}
-
-enum Status: String, CaseIterable {
-    case fresh = "Fresh"
-    case expiresSoon = "Expires Soon"
-    case expired = "Expired"
-}
-
-struct Donation {
-    let title: String
-    let category: Category
-    let location: Location
-    let status: Status
-}
