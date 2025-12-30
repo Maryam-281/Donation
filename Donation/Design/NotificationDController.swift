8442cca8ce711f4b6f648ab0bc9c2dbbff6cf35a
znbhassan <znbhassan23@gmail.com>
2025-12-30 16:15:23 +0300

    hiii

---
+++ Donation/Design/NotificationDController.swift
@@ -0,0 +1,30 @@
+//
+//  NotificationDController.swift
+//  Donation
+//
+//  Created by BP-36-201-01 on 30/12/2025.
+//
+
+import UIKit
+
+class NotificationDController: UIViewController {
+
+    @IBOutlet weak var O1: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Donation/Design/NotificationsController.swift
@@ -0,0 +1,30 @@
+//
+//  NotificationsController.swift
+//  Donation
+//
+//  Created by BP-36-201-01 on 30/12/2025.
+//
+
+import UIKit
+
+class NotificationsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var donationView: UIView!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
