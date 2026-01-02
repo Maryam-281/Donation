e64de26a7ae9bd9ceb90409544c48df17c8f37c7
maryam-281 <202300485@student.polytechnic.bh>
2026-01-02 16:35:42 +0300

    VIEW DETAILS WORKS FINALLY

--- Donation/Code/CardView.swift
+++ Donation/Code/CardView.swift
@@ -6,60 +6,34 @@
 //
 
 import UIKit
-
 class CardView: UIView {
 
-    // MARK: - Outlets (XIB)
     @IBOutlet weak var foodImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var expiryLabel: UILabel!
     @IBOutlet weak var distanceLabel: UILabel!
-    @IBOutlet weak var detailsButton: UIButton!
 
-    // MARK: - Data
     private var donation: Donations?
     var onDetailsTapped: ((Donations) -> Void)?
 
-    // MARK: - Intrinsic Size
-    override var intrinsicContentSize: CGSize {
-        return CGSize(width: UIView.noIntrinsicMetric, height: 260)
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        // ✅ THIS BYPASSES ALL BUTTON / STACKVIEW ISSUES
+        let tap = UITapGestureRecognizer(target: self, action: #selector(cardTapped))
+        addGestureRecognizer(tap)
+        isUserInteractionEnabled = true
     }
 
-    // MARK: - Layout
-    override func layoutSubviews() {
-        super.layoutSubviews()
-
-        layer.cornerRadius = 16
-        layer.masksToBounds = false
-        layer.applySketchShadow()
-    }
-
-    // MARK: - Configure
     func configure(with donation: Donations) {
         self.donation = donation
-
-        // Text
         titleLabel.text = donation.title
-        expiryLabel.text = formattedExpiry(from: donation.expirationDate)
-
-        // ✅ SHOW LOCATION (NOT "2 km")
+        expiryLabel.text = "Expires soon"
         distanceLabel.text = donation.location
-
-        // Image
-        foodImageView.image = UIImage(named: donation.imageName)
-        foodImageView.contentMode = .scaleAspectFill
-        foodImageView.clipsToBounds = true
     }
 
-    // MARK: - Helpers
-    private func formattedExpiry(from date: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        return "Expires: \(formatter.string(from: date))"
-    }
-
-    // MARK: - Action
-    @IBAction func detailsTapped(_ sender: UIButton) {
+    @objc private func cardTapped() {
+        print("🟢 CARD TAPPED")
         guard let donation = donation else { return }
         onDetailsTapped?(donation)
     }
